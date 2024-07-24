d92adeb6544532fda2077545928636b566ccd112
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-07-24 22:38:03 +0200

    add script to extract individual sprites

---
+++ scripts/skin-recolor/src/extract_vanilla_individual_sprites.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from pathlib import Path
+
+from PIL import Image
+from UnityPy.environment import Environment
+from UnityPy.classes import Sprite, SpriteAtlas
+from UnityPy.export.SpriteHelper import get_image
+from UnityPy.enums import ClassIDType
+import json
+
+path = Path("C:/Program Files (x86)/Steam/steamapps/common/Nine Sols/NineSols_Data/")
+out = Path("out/individual_sprites")
+out.mkdir(exist_ok=True)
+
+if not path.exists():
+    raise Exception(f"{path} does not exist")
+
+def interest_in_sprite(name: str):
+    return "Yee" in name and not name in ["HoHoYee_無極之地Bk"]
+
+atlases: set[SpriteAtlas] = set()
+
+# files = ["resources.assets", *[f"sharedassets{i}.assets" for i in range(0, 116)]]
+files = ["resources.assets"]
+
+for filename in files:
+    # load file
+    assets = Environment(str(path.joinpath(filename))).file
+
+    # iterate over every object in the file
+    for obj in assets.objects.values():
+        # if it's a sprite
+        if obj.type == ClassIDType.Sprite:
+            sprite: Sprite = obj.read()
+            sprite_name = sprite.m_Name
+
+            # if the sprite name interests us
+            if interest_in_sprite(sprite_name):
+                print(sprite_name)
+                # export png
+                path = out.joinpath(sprite_name).with_suffix(".png")
+                sprite.image.save(str(path))
