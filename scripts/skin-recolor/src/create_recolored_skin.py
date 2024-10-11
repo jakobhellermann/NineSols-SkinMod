aff385e39979ba30f2f60a083ef271243a4c28e7
Jakob Hellermann <jakob.hellermann@protonmail.com>
2024-10-11 23:34:00 +0200

    recolor: separate between necessary color replacements and weak ones

--- scripts/skin-recolor/src/create_recolored_skin.py
+++ scripts/skin-recolor/src/create_recolored_skin.py
@@ -32,7 +32,7 @@ def replace_color(data, target_color, new_color, fuzz):
 
 input_dir = Path("out/skin_vanilla")
 
-def recolor(name, out_dir, replacements, fuzz):
+def recolor(name, out_dir, replacements, replacements_weak, fuzz):
     out_dir.mkdir(exist_ok=True)
 
     with open(out_dir.joinpath("skin.json"), "w") as f:
@@ -44,7 +44,6 @@ def recolor(name, out_dir, replacements, fuzz):
         if atlas.is_file(): continue
 
         out_dir_atlas = out_dir.joinpath(atlas.name)
-        out_dir_atlas.mkdir(exist_ok=True)
 
         for sprite in atlas.iterdir():
             out_sprite = out_dir_atlas.joinpath(sprite.name)
@@ -55,10 +54,14 @@ def recolor(name, out_dir, replacements, fuzz):
                 data = np.array(img)
 
                 any_changed = False
-                for replace, to in replacements:
+                for replace, to, deciding_factor in replacements:
                     any_changed = any_changed or replace_color(data, replace, to, fuzz)
                 if any_changed:
+                    for replace, to, deciding_factor in replacements_weak:
+                        replace_color(data, replace, to, fuzz)
+
                     n_changed += 1
+                    out_dir_atlas.mkdir(exist_ok=True)
                     result_img = Image.fromarray(data, "RGBA")
                     result_img.save(out_sprite)
                 else:
@@ -68,8 +71,8 @@ def recolor(name, out_dir, replacements, fuzz):
     print(f"Changed ({name}): {n_changed}/{n_changed + n_unchanged}")
 
 
-def create_skin(name, out, replacements):
-    recolor(name, out, replacements, fuzz=50)
+def create_skin(name, out, replacements, replacements_weak):
+    recolor(name, out, replacements, replacements_weak, fuzz=50)
     shutil.make_archive(str(out), "zip", out)
 
 
@@ -80,11 +83,13 @@ create_skin(
     "red",
     out.joinpath(f"skin_red"),
     [
-        ((231, 174, 51), (231, 51, 60)),  # cape
-        ((66, 67, 49), (66, 53, 49)),  # cape border
-        ((115, 87, 27), (66, 53, 49)),  # cape shadow
-        ((168, 129, 40), (66, 53, 49)),  # cape shadow 2
-        ((82, 203, 132), (106, 125, 185)),  # green highlight
+        ((231, 174, 51), (231, 51, 60), True),  # cape
+    ],
+    [
+        ((66, 67, 49), (66, 53, 49), False),  # cape border
+        ((115, 87, 27), (66, 53, 49), False),  # cape shadow
+        ((168, 129, 40), (66, 53, 49), False),  # cape shadow 2
+        ((82, 203, 132), (106, 125, 185), False),  # green highlight
     ],
 )
 
@@ -92,10 +97,12 @@ create_skin(
     "purple",
     out.joinpath(f"skin_purple"),
     [
-        ((231, 174, 51), (91, 52, 235)),  # cape
-        ((66, 67, 49), (66, 53, 49)),  # cape border
-        ((115, 87, 27), (66, 53, 49)),  # cape shadow
-        ((168, 129, 40), (66, 53, 49)),  # cape shadow 2
-        ((82, 203, 132), (106, 125, 185)),  # green highlight
+        ((231, 174, 51), (91, 52, 235), True),  # cape
+    ],
+    [
+        ((66, 67, 49), (66, 53, 49), False),  # cape border
+        ((115, 87, 27), (66, 53, 49), False),  # cape shadow
+        ((168, 129, 40), (66, 53, 49), False),  # cape shadow 2
+        ((82, 203, 132), (106, 125, 185), False),  # green highlight
     ],
 )
